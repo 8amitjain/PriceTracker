6baf39a7ee1c1902dfa92b988ee1a03789c58515
amitj-coder <amitj-coder@users.noreply.github.com>
2020-08-15 20:11:18 +0530

    Multi products

--- tracker/spiders/tracker_spider.py
+++ tracker/spiders/tracker_spider.py
@@ -5,22 +5,40 @@ from ..items import TrackerItem
 
 class TrackerSpiderSpider(scrapy.Spider):
     name = 'tracker_spider'
-    # allowed_domains = ['amazon.com']
-    start_urls = ['https://www.amazon.in/gp/product/B07XVMDRZY/ref=s9_acss_bw_cg_Budget_3a1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-9&pf_rd_r=K1GJZ6HJCPQ90MCHDS0P&pf_rd_t=101&pf_rd_p=ac3235bd-aa25-433c-b15a-07f0f2c7eb2e&pf_rd_i=1389401031']
+    allowed_domains = ['amazon.com']
+    search_term = input("Enter Keyword to scrap: ")
+    start_urls = [f'https://www.amazon.in/s?k={search_term}']
 
     def parse(self, response):
         items = TrackerItem()
 
-        product_name = response.css('#productTitle::text').extract()
-        product_sold_by = response.css('#sellerProfileTriggerId::text').extract()
-        product_price = response.css('#priceblock_dealprice::text').extract()
-        product_image_link = response.css('#landingImage').extract()
-        text = str(product_image_link)
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
-        items['product_name'] = product_name
-        items['product_sold_by'] = product_sold_by
-        items['product_price'] = product_price
-        items['product_image_link'] = urls
+        product_title = response.css('.a-color-base.a-text-normal::text').extract()
+        product_total_number_rating = response.css('.a-size-small .a-size-base::text').extract()
+        product_price = response.css('.a-price-whole::text').extract()
+        product_image_link = response.css('.s-image::attr(src)').extract()
 
-        yield items
+        for i in range(len(product_title)):
+            try:
+                items['product_title'] = product_title[i].strip()
+                print(product_title[i].strip(), 'product_title')
+
+                if product_total_number_rating[i]:
+                    items['product_total_number_rating'] = product_total_number_rating[i].strip()
+                    print(product_total_number_rating[i].strip(), 'product_total_number_rating')
+
+                else:
+                    items['product_total_number_rating'] = 0
+
+                if product_price[i]:
+                    items['product_price'] = product_price[i].strip()
+                    print(product_price[i].strip(), 'product_price')
+                else:
+                    items['product_price'] = 0
+
+                items['product_image_link'] = product_image_link[i].strip()
+                print(product_image_link[i].strip(), 'product_image_link')
+
+            except IndexError:
+                pass
+            yield items
 
